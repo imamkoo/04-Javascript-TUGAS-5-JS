08ab9cc7f9049ff01456082a29ef84d6b34a50da
Imamkoo <imammm.kooo@gmail.com>
2021-12-14 12:47:53 +0700

    Tugas 5 Javascript

---
+++ tugas5.js
@@ -0,0 +1,11 @@
+for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0)
+        console.log("Bilangan kelipatan 3 :  " + i);
+
+}
+
+console.log("")
+
+for (let i = 1; i <= 20; i++) {
+    console.log("Bilangan ke : " + i);
+}
